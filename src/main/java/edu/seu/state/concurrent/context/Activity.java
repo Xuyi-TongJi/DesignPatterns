fa3118282e73d57086e019f248ce1a3293a4ef5c
Xuyi-TongJi <220210490@seu.edu.cn>
2021-12-19 23:14:35 +0800

    v 0.12.19

--- src/main/java/edu/seu/state/concurrent/context/Activity.java
+++ src/main/java/edu/seu/state/concurrent/context/Activity.java
@@ -1,19 +1,17 @@
 package edu.seu.state.concurrent.context;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Activity {
-    private final AtomicInteger count;
+    private int count;
 
     public Activity(Integer count) {
-        this.count = new AtomicInteger(count);
+        this.count = count;
     }
 
     public boolean getPrize() {
-        return count.decrementAndGet() <= 0;
+        return (--count) <= 0;
     }
 
     public int getCount() {
-        return count.get();
+        return count;
     }
 }
